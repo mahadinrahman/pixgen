7fd126a3b2a11309e5952a62966d2a919e607ade
mahadinrahman <mahadinrahman333@gmail.com>
2026-05-03 10:04:26 +0600

    link change

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient(process.env.AUTH_DB_URI);
 
 
 const db = client.db('pixgen');
